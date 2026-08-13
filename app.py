6f4cbcfc473c3018de64b3278fc9b50aa45d4e04
quietpen90-dev <quietpen90@gmail.com>
2026-08-13 16:43:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -413,9 +413,9 @@ class Handler(BaseHTTPRequestHandler):
 
     def do_POST(self):
 
-if self.path == "/register":
+        if self.path == "/register":
 
-    try:
+            try:
         length = int(
             self.headers.get("Content-Length", 0)
         )
